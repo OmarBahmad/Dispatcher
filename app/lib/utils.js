2f17b57cdf6c5540f41c789f8defb81be378fd54
Omar Bahmad <omar.bahmad@bideafactory.com>
2024-05-26 23:24:38 -0300

    chore: new connect props

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,19 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
 
 export const connecToDB = async () => {
   const connection = {};
-  const dotenv = require("dotenv");
-  dotenv.config({ path: "./config.env" });
-
-  const DB = process.env.DATABASE;
 
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(DB, {
-      useUnifiedTopology:true,
-      useNewUrlParser: true
-    });
+    const db = await mongoose.connect(process.env.MONGO, { useNewUrlParser: true });
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     throw new Error(error);
