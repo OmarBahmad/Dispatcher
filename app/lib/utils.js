f0584ff1e8b6fb4c30078ed689391f487cd73b52
Omar Bahmad <omar.bahmad@bideafactory.com>
2024-05-28 22:11:14 -0300

    chore: remove consoles

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-export const connecToDB = async () => {
-  const connection = {};
+const connection = {};
 
+export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO, { useNewUrlParser: true });
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
+    console.log(error)
     throw new Error(error);
   }
 };
