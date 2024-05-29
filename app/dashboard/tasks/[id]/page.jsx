bfc08fb4c310a11d932deba857de1f8953bedaf1
Omar Bahmad <omar.bahmad@bideafactory.com>
2024-05-29 02:55:13 -0300

    feature(tasks): Creating new connection and form to add tasksn

--- app/dashboard/tasks/[id]/page.jsx
+++ app/dashboard/tasks/[id]/page.jsx
@@ -1,33 +1,83 @@
+import { updateTask } from "@/app/lib/actions";
+import { fetchSingleTask } from "@/app/lib/data";
 import styles from "@/app/ui/dashboard/tasks/singleTask/singleTask.module.css";
-import Image from "next/image";
 
-const SingleTaskPage = () => {
+const SingleTaskPage = async ({ params }) => {
+  const { id } = params;
+  const task = await fetchSingleTask(id);
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
-        <form action="" className={styles.form}>
-          <input type="text" placeholder="Title" name="title" required />
-          <select name="cat" id="cat">
-            <option value="general"> Choose a Category</option>
-            <option value="option 1"> Option 1</option>
-            <option value="option 2"> Option 2</option>
-            <option value="option 3"> Option 3</option>
-          </select>
-          <input
-            type="number"
-            placeholder="Driver Licence"
-            name="driverlicence"
-          />
-          <input type="text" placeholder="License Plate" name="licenseplate" />
-          <input type="text" placeholder="Driver Name" name="drivername" />
-          <input type="text" placeholder="Car" name="car" />
-          <textarea
-            name="desc"
-            id="desc"
-            rows="16"
-            placeholder="Description"
-          ></textarea>
-          <button type="submit">Submit</button>
+        <form action={updateTask} className={styles.form}>
+          <input type="hidden" name="id" value={task.id} />
+          <div>
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Task title"
+              defaultValue={task.title}
+            />
+          </div>
+          <div>
+            <label>Category</label>
+            <select name="cat" id="cat">
+              <option value={task.cat} defaultValue={task.cat}>
+                {" "}
+                {task.cat}
+              </option>
+              <option value="open"> Open</option>
+              <option value="close"> Close</option>
+              <option value="standby"> StandBy</option>
+            </select>
+          </div>
+          <div>
+            <label>Driver Licence</label>
+            <input
+              type="text"
+              name="driverlicence"
+              placeholder="Driver Licence"
+              defaultValue={task.driverlicence}
+            />
+          </div>
+          <div>
+            <label>License Plate</label>
+            <input
+              type="text"
+              name="licenseplate"
+              placeholder="License Plate"
+              defaultValue={task.licenseplate}
+            />
+          </div>
+          <div>
+            <label>Driver Name</label>
+            <input
+              type="text"
+              name="drivername"
+              placeholder="Driver Name"
+              defaultValue={task.drivername}
+            />
+          </div>
+          <div>
+            <label>Car</label>
+            <input
+              type="text"
+              name="car"
+              placeholder="Car"
+              defaultValue={task.car}
+            />
+          </div>
+          <div>
+            <label>Description</label>
+            <textarea
+              type="text"
+              name="desc"
+              placeholder="Description"
+              defaultValue={task.desc}
+            />
+          </div>
+          <button type="submit">Update</button>
         </form>
       </div>
     </div>
